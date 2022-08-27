f45e77a5a476906de6a00dad6c5d5340a3b75c24
Jay <iamjdpatel@gmail.com>
2022-08-27 13:04:10 +0530

    12 User Defaults

---
+++ 12/NamesToFacesB/NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Jay on 8/27/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
