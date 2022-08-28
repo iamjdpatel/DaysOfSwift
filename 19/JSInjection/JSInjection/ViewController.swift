fc57bdd21ace6d36dff60b34f1fbb6cbe789cc1b
Jay <iamjdpatel@gmail.com>
2022-08-28 16:43:45 +0530

    19 JavaScript Injection

---
+++ 19/JSInjection/JSInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSInjection
+//
+//  Created by Jay on 8/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
